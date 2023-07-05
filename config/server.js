164ef67831be297b4973c0c4f2fb54cb57acaee4
dimashimko <dimashimko7@gmail.com>
2023-07-05 10:19:05 +0300

    init commit

---
+++ config/server.js
@@ -0,0 +1,15 @@
+module.exports = ({ env }) => ({
+  jwt: {
+    secret: env('JWT_SECRET', 'my-secret'),
+    expiresIn: '30s',
+  },
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  app: {
+    keys: env.array('APP_KEYS'),
+  },
+  webhooks: {
+    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+  },
+});
+
