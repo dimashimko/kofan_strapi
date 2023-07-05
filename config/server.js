d7055047f298b482308115cbc6061a009856c56b
dimashimko <dimashimko7@gmail.com>
2023-07-05 13:51:38 +0300

    change jwt expiresIn to 30d

--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   jwt: {
     secret: env('JWT_SECRET', 'my-secret'),
-    expiresIn: '30s',
+    expiresIn: '30d',
   },
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
